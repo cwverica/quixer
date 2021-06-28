b3991a47f22df2c4ad9de6caa97ce25feb570257
cwverica <cwverica@gmail.com>
2021-06-27 19:53:05 -0700

    updated env variable

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = os.environ.get('DEBUG_TB_INTERCEPT_REDIRECTS', True)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', SECRET_KEY)
 toolbar = DebugToolbarExtension(app)
 
